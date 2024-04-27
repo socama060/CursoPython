957367e79c8033ee83eee7ed38a3c19b5298ce60
socama060 <socama060@gmail.com>
2024-04-27 09:54:15 +0200

    EjemploSuma

--- HolaMundo.py
+++ HolaMundo.py
@@ -1,1 +1,15 @@
 print("¡Hola, Mundo!")
+# Pedimos al usuario que ingrese el primer número
+numero1 = input("Ingrese el primer número: ")
+# Pedimos al usuario que ingrese el segundo número
+numero2 = input("Ingrese el segundo número: ")
+
+# Convertimos los números de cadena a entero
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+# Calculamos la suma de ambos números
+suma = numero1 + numero2
+
+# Imprimimos el resultado
+print("La suma de", numero1, "y", numero2, "es", suma)
